3856c769c0500464501ba1f38a85c358e89e7c4a
ooronning <christopher.a.ronning@gmail.com>
2020-02-01 17:43:32 -0500

    create trigram from text

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,39 @@ import path from 'path';
 
 const asyncFs = fs.promises;
 
+function createTrigram(text: string): Map<string, string[]> {
+  const textArray = text.split(' ');
+  const trigram = new Map<string, string[]>();
+
+  for (let i = 0; i < textArray.length; i++) {
+    if (!(textArray[i + 1] && textArray[i + 2])) {
+      break;
+    }
+
+    const key = `${textArray[i]} ${textArray[i + 1]}`;
+    const value = trigram.get(key);
+
+    if (value) {
+      value.push(textArray[i + 2]);
+    } else {
+      trigram.set(key, [textArray[i + 2]]);
+    }
+  }
+
+  return trigram;
+}
+
 async function run() {
   const text = await asyncFs.readFile(
     path.resolve(__dirname, '..', 'txt-source', 'test.txt'),
     { encoding: 'utf8' }
   );
-  console.log(text);
+  console.log('Input text: ', text);
+
+  const trigram = createTrigram(text);
+  for (const [key, value] of trigram.entries()) {
+    console.log(`${key} -> ${value}`);
+  }
 }
 
 run();
