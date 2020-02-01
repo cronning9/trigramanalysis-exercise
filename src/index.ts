4ad446763b03cd64aa55a1b7d192d13d6f231434
ooronning <christopher.a.ronning@gmail.com>
2020-02-01 18:31:08 -0500

    handle newlines

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,32 @@ import path from 'path';
 
 const asyncFs = fs.promises;
 
-function createTrigram(text: string): Map<string, string[]> {
-  const textArray = text.split(' ');
+function createTrigram(text: string[]): Map<string, string[]> {
   const trigram = new Map<string, string[]>();
 
-  for (let i = 0; i < textArray.length; i++) {
-    if (!(textArray[i + 1] && textArray[i + 2])) {
+  for (let i = 0; i < text.length; i++) {
+    if (!(text[i + 1] && text[i + 2])) {
       break;
     }
 
-    const key = `${textArray[i]} ${textArray[i + 1]}`;
+    const key = `${text[i]} ${text[i + 1]}`;
     const value = trigram.get(key);
 
     if (value) {
-      value.push(textArray[i + 2]);
+      value.push(text[i + 2]);
     } else {
-      trigram.set(key, [textArray[i + 2]]);
+      trigram.set(key, [text[i + 2]]);
     }
   }
 
   return trigram;
+}
+
+function handleInputText(text: string): string[] {
+  const array = text.split(/\s/);
+  // Splitting on newlines results in empty strings in the array.
+  // let's strip those out.
+  return array.filter(str => str !== '');
 }
 
 async function run() {
@@ -30,9 +36,9 @@ async function run() {
     path.resolve(__dirname, '..', 'txt-source', 'test.txt'),
     { encoding: 'utf8' }
   );
-  console.log('Input text: ', text);
+  console.log('Input text: ', handleInputText(text));
 
-  const trigram = createTrigram(text);
+  const trigram = createTrigram(handleInputText(text));
   for (const [key, value] of trigram.entries()) {
     console.log(`${key} -> ${value}`);
   }
