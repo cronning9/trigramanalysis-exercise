5e86612b5b17dc05a08b3a9b25f60f7dbce4f6e2
ooronning <christopher.a.ronning@gmail.com>
2020-02-03 22:26:11 -0500

    ensure we begin on a capital and end on punctuation

--- src/lib/generateText.ts
+++ src/lib/generateText.ts
@@ -1,6 +1,8 @@
 import { TrigramTable } from '../types';
 import getRandomNumber from './generateRandom';
 
+const PUNC = ['.', '!', '?'];
+
 function selectNextWord(pair: string, trigramTable: TrigramTable) {
   const values = trigramTable.get(pair);
   if (!values) {
@@ -14,6 +16,9 @@ export default function generateText(trigramTable: TrigramTable, maxLength: numb
   const output: string[] = [];
 
   let pair = keys[getRandomNumber(keys.length)];
+  while (!/[A-Z]/.test(pair[0])) {
+    pair = keys[getRandomNumber(keys.length)];
+  }
   let nextWord = selectNextWord(pair, trigramTable);
   if (!nextWord) {
     throw new Error('Something strange has happened.');
@@ -21,18 +26,40 @@ export default function generateText(trigramTable: TrigramTable, maxLength: numb
   pair.split(' ').forEach(str => output.push(str));
   output.push(nextWord);
 
-  // search for a pair that matches the last two words in the string
-  // if it exists in the trigramTable, select a random next character.
-  // else, return;
+  /**
+   * Search for a pair that matches the last two words in the string
+   * if it exists in the trigramTable, select a random next character.
+   * else, return;
+   *
+   * Once we've reached that maxLength, we want to continue until we're
+   * ending on punctuation.
+   */
   while (output.length <= maxLength) {
     pair = `${output[output.length - 2]} ${output[output.length - 1]}`;
     nextWord = selectNextWord(pair, trigramTable);
     if (!nextWord) {
-      return output.join(' ');
+      return PUNC.includes(pair[pair.length - 1]) ? output.join(' ') : output.join(' ') + '.';
     }
     output.push(nextWord);
   }
 
   // TODO: consider punctuation.
+  if (PUNC.includes(pair[pair.length - 1])) {
+    return output.join(' ');
+  } else {
+    let ending = false;
+    while (!ending) {
+      pair = `${output[output.length - 2]} ${output[output.length - 1]}`;
+      nextWord = selectNextWord(pair, trigramTable);
+      if (!nextWord) {
+        return PUNC.includes(pair[pair.length - 1]) ? output.join(' ') : output.join(' ') + '.';
+      }
+      output.push(nextWord);
+      if (PUNC.includes(nextWord[nextWord.length - 1])) {
+        ending = true;
+      }
+    }
+  }
+
   return output.join(' ');
 }
